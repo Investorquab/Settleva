2ef1bd1cc4ee5d8646ead14fe9a519bb46e274e7
quabnation <quadribello3@gmail.com>
2026-09-23 14:16:50 +0100

    Fix provider test import

--- packages/providers/src/github-deployment.test.ts
+++ packages/providers/src/github-deployment.test.ts
@@ -1,5 +1,5 @@
 import { strict as assert } from "node:assert";
-import test from "node:test";
+import { test } from "node:test";
 import {
   GITHUB_DEPLOYMENT_CLAIM_FIELDS,
   buildGitHubDeploymentCondition
