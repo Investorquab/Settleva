875f313d5cda3b28fefe84b8ffcd072eecfa3a5e
quabnation <quadribello3@gmail.com>
2026-09-23 14:05:47 +0100

    Export provider implementations and GitHub deployment schema

---
+++ packages/providers/src/index.ts
@@ -0,0 +1,7 @@
+export { GitHubProofProvider } from "./github.js";
+export { HttpProofProvider } from "./http.js";
+export {
+  GITHUB_DEPLOYMENT_CLAIM_FIELDS,
+  buildGitHubDeploymentCondition
+} from "./github-deployment.js";
+export type { GitHubDeploymentConditionInput } from "./github-deployment.js";
