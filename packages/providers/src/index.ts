43f34a34954a4d67e1ffe73ebbb0d6f70052c9ed
quabnation <quadribello3@gmail.com>
2026-09-24 02:56:08 +0100

    Export strict GitHub deployment evidence helper

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -2,6 +2,7 @@ export { GitHubProofProvider } from "./github.js";
 export { HttpProofProvider } from "./http.js";
 export {
   GITHUB_DEPLOYMENT_CLAIM_FIELDS,
-  buildGitHubDeploymentCondition
+  buildGitHubDeploymentCondition,
+  extractGitHubDeploymentClaims
 } from "./github-deployment.js";
-export type { GitHubDeploymentConditionInput } from "./github-deployment.js";
+export type { GitHubDeploymentConditionInput, GitHubDeploymentVerifiedClaims } from "./github-deployment.js";
